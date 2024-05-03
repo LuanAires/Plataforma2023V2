30b092628a7ab95c1f52c8fc4db1a9c8a7ef4c33
CCGstudios <luan.cerario@gmail.com>
2024-05-03 18:53:55 -0300

    ponto de origem de ataque sapo vermelho, sapo azul e fantasma

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -17,7 +17,7 @@ public class SceneManagerScript : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Collision.
+        
         {
         
         
