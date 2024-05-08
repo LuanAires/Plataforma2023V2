9339885f90a5b11f916e3d8d30b76e4823cf5d67
Luan Aires <luan.cerario@gmail.com>
2024-05-08 09:30:48 -0300

    tremendo

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private CinemachineVirtualCamera CinemachineVirtualcamera;
+    private float ShakeIntensity = 1.0f;
+    private float ShakeTime = 0.2f;
+    private float timer;
+    private CinemachineBasicMultiChannelPerlin _cbmcp;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        CinemachineVirtualcamera= GetComponent<CinemachineVirtualCamera>();
+    }
+    private void Start()
+    {
+        CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualcamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cbmcp.m_AmplitudeGain = ShakeIntensity;
+        timer = ShakeTime;
+    }
+    private void StopShake()
+    {
+        CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualcamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cbmcp.m_AmplitudeGain -= 0f;
+        timer = 0;
+    }
     void Update()
     {
         
