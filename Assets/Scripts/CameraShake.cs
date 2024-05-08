0f4c6b43acdbde5fcb6b1c4e5b878d649eb0f599
CCGstudios <luan.cerario@gmail.com>
2024-05-08 17:35:37 -0300

    script+ screem shake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Unity.VisualScripting;
 
 public class CameraShake : MonoBehaviour
 {
@@ -10,12 +11,18 @@ public class CameraShake : MonoBehaviour
     private float ShakeTime = 0.2f;
     private float timer;
     private CinemachineBasicMultiChannelPerlin _cbmcp;
+    private SalaBoss sala;
+
 
     private void Awake()
     {
         CinemachineVirtualcamera= GetComponent<CinemachineVirtualCamera>();
     }
     private void Start()
+    {
+        StopShake();
+    }
+    public void ShakeCamera()
     {
         CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualcamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         _cbmcp.m_AmplitudeGain = ShakeIntensity;
@@ -29,6 +36,19 @@ public class CameraShake : MonoBehaviour
     }
     void Update()
     {
-        
+       if(Input.GetKeyDown(KeyCode.Escape)) 
+       {
+            ShakeCamera();
+       }
+        if (timer > 0) 
+        {
+          timer -= Time.deltaTime;
+
+            if(timer <= 0)  
+            {
+              StopShake();
+            
+            }
+        }
     }
 }
