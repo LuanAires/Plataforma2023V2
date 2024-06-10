09708c9f3aa5bc558d1b5458736e6ac5011eb72e
CCGstudios <luan.cerario@gmail.com>
2024-06-10 19:43:07 -0300

    lá vamos nos

--- Assets/Scripts/HudControlles/HudBossController.cs
+++ Assets/Scripts/HudControlles/HudBossController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class HudBossController : MonoBehaviour
 {
+    public int maxlife;
+    public int currentylife;
+    [SerializeField] private float multiplier;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = new Vector3(currentylife * multiplier / maxlife, 1, 1);
     }
 }
+
+
